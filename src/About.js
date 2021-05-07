5a44b2f2133efc256b04a2a36125ed62dc19465a
shamimarahman89 <61672265+shamimarahman89@users.noreply.github.com>
2021-05-07 17:42:03 -0400

    Update About.js

--- src/About.js
+++ src/About.js
@@ -7,7 +7,7 @@ export default function About() {
         <Card.Body>
             <Card.Title> About </Card.Title>
             <Card.Text> 
-              I am a recent computer programming graduate from Seneca College with fundamental knowledge of software design and development and actively looking for entry-level software development opportunities.
+              I am Shamima Rahman, a recent computer programming graduate from Seneca College with fundamental knowledge of software design and development and actively looking for entry-level software development opportunities.
 I am experienced in the development of front-end software components using modern JavaScript frameworks and libraries like Angular, React, and appropriate HTML, CSS that goes with it.
 I also have experience in developing backend software components and REST APIs using Express.js, Node.js having appropriate integration with SQL and NoSQL Databases like Oracle, PostgreSQL, and MongoDB.
 Seeking to utilize my educational background with my analytical, technical, and programming skills to thrive as a software developer.
